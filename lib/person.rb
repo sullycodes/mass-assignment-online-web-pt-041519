3c1064b03a2b0f86ca8e672984983e0ce2b179fc
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-29 01:06:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    # self.send{|k,y| {k= }, {y})
+    attributes.each {|k, v| self.send(("#{key}="), value)}  
   end
   
 end
